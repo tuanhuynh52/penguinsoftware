4bc1caee3f806f8cfcfb220dbfc4457e13725d75
tuanhuynh52 <tuanhuynh5290@gmail.com>
2015-04-28 16:52:23 -0700

    testing commit file to Git

---
+++ GitTest/src/main.java
@@ -0,0 +1,16 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		displayMessage();
+	}
+
+	private static void displayMessage() {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Penguin Software Team!!!!");
+		
+	}
+	
+
+}
